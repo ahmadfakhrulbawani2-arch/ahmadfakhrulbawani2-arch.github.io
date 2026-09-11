046d84bd935b703f90fb8f7e33a44db5012c072a
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2026-09-11 10:47:41 +0700

    feat: topbar and sidebar done

---
+++ script.js
@@ -0,0 +1,35 @@
+// dom
+const hamburgerBtn = document.getElementById('hamburgerBtn');
+const mobileSidebar = document.getElementById('mobileSidebar');
+const sidebarLinks = document.querySelectorAll('.sidebar-link');
+const sidebarOverlay = document.getElementById('sidebarOverlay');
+const body = document.body;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleMenu = () => {
+    // tambahkan jika tidak ada, hapus jika ada
+    hamburgerBtn.classList.toggle('active');
+    mobileSidebar.classList.toggle('open');
+    sidebarOverlay.classList.toggle('open');
+
+    // jika sidebar terbuka matikan scroller
+    if (mobileSidebar.classList.contains('open')) {
+      body.style.overflow = 'hidden';
+    } else {
+      body.style.overflow = '';
+    }
+  };
+
+  // tambahkan onClick hamburgerBtn dengan toggleMenu
+  hamburgerBtn.addEventListener('click', toggleMenu);
+  sidebarOverlay.addEventListener('click', toggleMenu);
+
+  // kita ingin ketika klik link otomatis tertutup jadi tambahkan ke tiap link
+  sidebarLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      if (mobileSidebar.classList.contains('open')) {
+        toggleMenu();
+      }
+    });
+  });
+});
