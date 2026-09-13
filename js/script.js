142084bb911cf32b52c817d4cdd1910adb094e5b
ahmadfakhrulbawani2-arch <ahmadfakhrulbawani2@gmail.com>
2026-09-13 07:42:19 +0700

    feat: add about me section and lenis

--- js/script.js
+++ js/script.js
@@ -31,3 +31,14 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+// lenis
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
