c4b890630d53c92d79ecdf1a973ae11f9939a713
ahmadfakhrulbawani2-arch <ahmadfakhrulbawani2@gmail.com>
2026-09-13 12:16:30 +0700

    fix: mobile navbar lenis stop and enhance responsiveness

--- js/script.js
+++ js/script.js
@@ -9,6 +9,22 @@ const mobileSidebar = document.getElementById('mobileSidebar');
 const sidebarLinks = document.querySelectorAll('.sidebar-link');
 const sidebarOverlay = document.getElementById('sidebarOverlay');
 const body = document.body;
+const scrollProgressBar = document.getElementById('scrollProgressBar');
+const backToTop = document.getElementById('backToTop');
+
+// lenis
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+  anchors: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', ({ progress, scroll }) => {
+  topbar.classList.toggle('scrolled', scroll > 100);
+  backToTop.classList.toggle('show', scroll > 100);
+  scrollProgressBar.style.width = `${progress * 100}%`;
+});
 
 const toggleMenu = () => {
   hamburgerBtn.classList.toggle('active');
@@ -17,8 +33,10 @@ const toggleMenu = () => {
 
   if (mobileSidebar.classList.contains('open')) {
     body.style.overflow = 'hidden';
+    lenis.stop();
   } else {
     body.style.overflow = '';
+    lenis.start();
   }
 };
 
@@ -27,13 +45,4 @@ sidebarOverlay.addEventListener('click', toggleMenu);
 
 sidebarLinks.forEach((link) => {
   link.addEventListener('click', toggleMenu);
-});
-
-const lenis = new Lenis({
-  autoRaf: true,
-  anchors: true,
-});
-
-lenis.on('scroll', () => {
-  topbar.classList.toggle('scrolled', lenis.scroll > 100);
 });
